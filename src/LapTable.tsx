9762c1f88f1562e1d94c2b647a484f83c3e913c4
kotasimo <kotasimo31@gmail.com>
2026-04-18 16:36:59 +0900

    ラップ履歴の色変え

--- src/LapTable.tsx
+++ src/LapTable.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import type { Lap } from "./type";
 
 type Props = {
@@ -5,7 +6,20 @@ type Props = {
   formatTimeText: (ms: number) => string;
 };
 
-export const LapTable = ({ laps, formatTimeText}: Props) => {
+export const LapTable = ({ laps, formatTimeText }: Props) => {
+  const [selectedLap, setSelectedLap] = useState<number | null>(() => {
+    const saved = localStorage.getItem("selectedLap");
+    return saved ? Number(saved) : null;
+  });
+
+  useEffect(() => {
+    if (selectedLap !== null) {
+      localStorage.setItem("selectedLap", String(selectedLap));
+    } else {
+      localStorage.removeItem("selectedLap");
+    }
+  }, [selectedLap]);
+
   return (
 
     <div className="max-h-88 text-center overflow-auto border-t border-white/10 ">
@@ -17,10 +31,17 @@ export const LapTable = ({ laps, formatTimeText}: Props) => {
             <th className="color px-1 py-2 text-center font-medium">Total</th>
           </tr>
         </thead>
-        
+
         <tbody className="divide-y divide-white/10">
           {laps.map((lap) => (
-            <tr className= "bg-white/7" key={lap.lap}>
+            <tr
+              key={lap.lap}
+              onClick={() =>
+                setSelectedLap((prev) => (prev === lap.lap ? null : lap.lap))
+              }
+              className={`cursor-pointer transition-colors active:bg-green-700 ${selectedLap === lap.lap ? "bg-green-700" : "bg-white/7"
+                }`}
+            >
               <td className="px-2 py-2 text-center text-slate-200">{lap.lap}</td>
               <td className="px-2 py-2 text-center font-mono tabular-nums text-slate-100">
                 {formatTimeText(lap.lapTime)}
